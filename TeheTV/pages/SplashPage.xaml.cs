ec797af0a58cb8033b3a9552a4f95ee2528c9504
DalBrar <31530959+DalBrar@users.noreply.github.com>
2018-11-27 19:05:54 -0700

    splash screen finished, checks if profiles.ini file exists and goes to ProfileSelector or Initialize accordingly.

--- TeheTV/pages/SplashPage.xaml.cs
+++ TeheTV/pages/SplashPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,34 +21,41 @@ namespace TeheTV.pages
     public partial class SplashPage : Page
     {
         MainWindow app;
+        SoundPlayer player;
+
         public SplashPage(MainWindow instance)
         {
             app = instance;
             InitializeComponent();
+
+            Stream strm = Properties.Resources.TeheTVwav;
+            player = new SoundPlayer(strm);
+            player.Load();
+
             this.Loaded += animateSplash;
         }
 
         private void animateSplash(object sender, RoutedEventArgs e)
         {
-            System.Windows.Media.Animation.Storyboard sb = this.FindResource("BGfadeIn") as System.Windows.Media.Animation.Storyboard;
-            System.Windows.Media.Animation.Storyboard.SetTarget(sb, this.blackBGD);
-            sb.Completed += FadeIn_Completed;
+            Storyboard sb = this.FindResource("BGfadeIn") as Storyboard;
+            sb.Completed += FadeOut;
             sb.Begin();
         }
 
-        private void FadeIn_Completed(object sender, EventArgs e)
+        private void FadeOut(object sender, EventArgs e)
         {
-            System.Windows.Media.Animation.Storyboard sb = this.FindResource("BGfadeOut") as System.Windows.Media.Animation.Storyboard;
-            System.Windows.Media.Animation.Storyboard.SetTarget(sb, this.blackBGD);
-            sb.Completed += FadeOut_Complete;
+            player.Play();
+            Storyboard sb = this.FindResource("BGfadeOut") as Storyboard;
+            sb.Completed += ExitSplash;
             sb.Begin();
         }
 
-        private void FadeOut_Complete(object sender, EventArgs e)
+        private void ExitSplash(object sender, EventArgs e)
         {
-            
+            if (File.Exists("profiles.ini"))
+                app.changeScreen(SCREEN.ProfileSelector);
+            else
+                app.changeScreen(SCREEN.Initialize);
         }
-
-        //  Style="{DynamicResource BGAnimation}"
     }
 }
