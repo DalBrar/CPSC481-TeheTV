5bace452caed2616211bc8bc0717f33536a3e69d
DalBrar <31530959+DalBrar@users.noreply.github.com>
2018-12-01 12:48:00 -0700

    updated Navigator & NaviTemplate to use Carolines layout design.

--- TeheTV/pages/Navigator.xaml.cs
+++ TeheTV/pages/Navigator.xaml.cs
@@ -32,23 +32,22 @@ namespace TeheTV.pages
         }
 
         /// <summary>
-        /// This can be used statically from any page as long as you pass it the "navi" instance variable.
         /// Use this to change the Navigation from the home row buttons
         /// </summary>
         /// <param name="instance"></param>
         /// <param name="navi"></param>
-        public static void changeNavigationFrame(MainWindow instance, NAVI navi)
+        public void changeNavigationFrame(NAVI navi)
         {
             if (navi == NAVI.Recommendations)
-                instance.ScreenFrame.NavigationService.Navigate(template);
+                naviFrame.NavigationService.Navigate(template);
             if (navi == NAVI.YouTube)
-                instance.ScreenFrame.NavigationService.Navigate(template);
+                naviFrame.NavigationService.Navigate(template);
             if (navi == NAVI.Netflix)
-                instance.ScreenFrame.NavigationService.Navigate(template);
+                naviFrame.NavigationService.Navigate(template);
             if (navi == NAVI.Spotify)
-                instance.ScreenFrame.NavigationService.Navigate(template);
+                naviFrame.NavigationService.Navigate(template);
             if (navi == NAVI.Games)
-                instance.ScreenFrame.NavigationService.Navigate(template);
+                naviFrame.NavigationService.Navigate(template);
         }
     }
 }
